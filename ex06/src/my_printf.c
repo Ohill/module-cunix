1307f92a8b276b32743d73d8dc10ba066ff867ec
Yaroslav Bond <ybondur@heliostech.fr>
2018-06-15 14:17:48 +0300

    Update my_printf.c

--- ex06/src/my_printf.c
+++ ex06/src/my_printf.c
@@ -4,19 +4,15 @@
 
 int my_atoi(const char* nptr) {
 	int res = 0;
-	while (isspace((unsigned char)*nptr)) {
+	while (isspace((unsigned char)*nptr))
 		nptr++;
-	}
 	char sign = *nptr;
-	if (*nptr == '-' || *nptr == '+') {
+	if (*nptr == '-' || *nptr == '+')
 		nptr++;
-	}
-	while (isdigit((unsigned char)*nptr)) {
+	while (isdigit((unsigned char)*nptr)) 
 		res = res * 10 - (*nptr++ - '0');
-	}
-	if (sign != '-') {
+	if (sign != '-') 
 		res = -res;
-	}
 	return res;
 }
 
