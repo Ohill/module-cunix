42ef8da0edf946bafd9063ad3dd4fd964245bdd5
Yaroslav Bond <ohilltrick@gmail.com>
2018-06-15 15:01:46 +0300

    memory fix

--- ex07/src/linked_list.c
+++ ex07/src/linked_list.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 node_t  *list_create(void *data)
 {
   node_t *head = (node_t *) malloc(sizeof(node_t)) ;
@@ -28,6 +29,7 @@ void    list_push(node_t *head, void *data)
   cur_elem->next->data = data;
   cur_elem->next->next = NULL;
 }
+
 void    list_unshift(node_t **head, void *data)
 {
   node_t *tmp = (node_t *) malloc (sizeof(node_t));
@@ -35,29 +37,36 @@ void    list_unshift(node_t **head, void *data)
   tmp->next = *head;
   *head = tmp;
 }
+
 void   *list_pop(node_t **head)
 {
   node_t *cur_elem = *head;
   while (cur_elem->next->next != NULL)
     cur_elem = cur_elem->next;
+  free(cur_elem->next->data);
   free(cur_elem->next);
   cur_elem->next = NULL;
 }
+
 void   *list_shift(node_t **head)
 {
   node_t *tmp = *head;
   *head = tmp->next;
+  free(tmp->data);
   free(tmp);
 }
+
 void   *list_remove(node_t **head, int pos)
 {
   node_t *cur_elem = *head;
-  for(int i = 0; i < pos; i++)
+  for(int i = 0; i < pos-1; i++)
     cur_elem = cur_elem->next;
   node_t *tmp = cur_elem->next;
   cur_elem->next = tmp->next;
+  free(tmp->data);
   free(tmp);
 }
+
 void    list_print(node_t *head)
 {
   node_t *tmp = head;
@@ -66,6 +75,7 @@ void    list_print(node_t *head)
       tmp = tmp->next;
   }
 }
+
 void    list_visitor(node_t *head, void (*fp)(void *data))
 {
  node_t *cur_elem = head;
