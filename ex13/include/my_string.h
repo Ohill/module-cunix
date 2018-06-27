57cee9248149b44fa82511da4e389bc7eaec4267
Yaroslav Bond <ybondur@heliostech.fr>
2018-06-27 23:33:56 +0300

    Update my_string.h

--- ex13/include/my_string.h
+++ ex13/include/my_string.h
@@ -1,12 +1,13 @@
 #ifndef _STRING_H_
 #define _STRING_H_
+
 #include "filler.h"
 
 #define BUF_SIZE 64
 
-stream_t        *string_init();
-stream_t        *string_create(char* str);
-void            string_append(stream_t *ptr, char* str);
-void            string_destroy(stream_t *str);
+stream_t *string_init();
+stream_t *string_create(char* str);
+void string_append(stream_t *ptr, char* str);
+void string_destroy(stream_t *str);
 
 #endif
