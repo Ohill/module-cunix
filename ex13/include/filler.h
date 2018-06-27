59931ed27d5f6ae157c3ec30624a26e3237515f0
Yaroslav Bond <ybondur@heliostech.fr>
2018-06-27 23:31:20 +0300

    Update filler.h

--- ex13/include/filler.h
+++ ex13/include/filler.h
@@ -55,8 +55,8 @@ typedef struct  strategy_s
 }               strategy_t;
 
 /*Functions for reading*/
-req_t*          read_request(filler_t *filler);
-void            read_input(filler_t* filler);
+req_t *read_request(filler_t *filler);
+void read_input(filler_t* filler);
 
 /*Functions for strategy */
 strategy_t *strategy_init();
@@ -69,28 +69,27 @@ void check_all(strategy_t *strategy);
 
 
 /*Functions for parsing*/
-req_t*          parse_all(char *all);
-pos_t           parse_size(char *answer);
-int             find_size(char *dist, char *source, int start);
+req_t *parse_all(char *all);
+pos_t parse_size(char *answer);
+int find_size(char *dist, char *source, int start);
 
-map_t      map_init(int width, int height);
-map_t       map_read(char *source, int pos, int w, int h);
-void            map_destroy(map_t *map);
+map_t map_init(int width, int height);
+map_t map_read(char *source, int pos, int w, int h);
+void map_destroy(map_t *map);
 
 /*Functions for game logic*/
-void            start_game(filler_t *filler);
-pos_t           play(strategy_t *strategy, req_t *core);
+void start_game(filler_t *filler);
+pos_t play(strategy_t *strategy, req_t *core);
 
 /*Functions for printing*/
-void            print_pos(pos_t p);
+void print_pos(pos_t p);
 
 /*Tools*/
 int check_free_space(map_t *map, map_t *new_elem, pos_t p);
 int check_connection(map_t *map, map_t *new_elem, pos_t pos, char symbol);
-int             set_nonblocking(int fd);
-void            fatal(char *msg);
-void            create_filler(filler_t *filler);
-void            destroy_filler(filler_t *filler);
-void            destroy_req(req_t *req);
-void            printlog(const char *filename, const char *mode, const char *format, ...);
+int set_nonblocking(int fd);
+void fatal(char *msg);
+void сreate_filler(filler_t *filler);
+void destroy_filler(filler_t *filler);
+void destroy_req(req_t *req);
 #endif // _FILLER_H_
